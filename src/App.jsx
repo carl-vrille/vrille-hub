9772d721f652708e8fce0cc885967c10592fcdf2
carl-vrille <carl@vrille.io>
2021-12-31 18:29:16 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,26 @@ import Menu from "./components/menu/Menu";
 import "./app.scss";
 import { useState } from "react";
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3uWtV6Mh-yXcIWCczBiU7aRUfw4hzkco",
+  authDomain: "vrille-hub.firebaseapp.com",
+  projectId: "vrille-hub",
+  storageBucket: "vrille-hub.appspot.com",
+  messagingSenderId: "949111706922",
+  appId: "1:949111706922:web:72ae12cb7fee5b3adfba53",
+  measurementId: "G-HGPYSEZV9T"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false);
